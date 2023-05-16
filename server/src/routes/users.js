699b603b8faa17c6512498107c1f1e00df6eee4e
polilotas123 <layyagami9@gmail.com>
2023-05-16 13:32:49 -0500

    proyecto para empezar

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Holaaaa");
-});
+  console.log("sad")
+  res.send("dsad")
+})
 
 module.exports = router;
