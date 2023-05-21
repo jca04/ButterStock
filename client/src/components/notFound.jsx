1bdab51e76f12e1ef28aaf3414c8b4e6ff7f4afe
polilotas123 <layyagami9@gmail.com>
2023-05-21 14:50:37 -0500

    instalacion mysql  y formik

--- client/src/components/notFound.jsx
+++ client/src/components/notFound.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function NotFound(){
+
   return(
-    <div><h2>No se encontro</h2></div>
+    <div><h2>No se encontro</h2><Link to="/about">About</Link></div>
   )
 }
 
