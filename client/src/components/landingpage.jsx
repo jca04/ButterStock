14b8d5447af4889197787c90608483e47c1ffbfc
Juan Parra <juangui902@gmail.com>
2023-08-21 09:23:35 -0500

    animations landingpage

--- client/src/components/landingpage.jsx
+++ client/src/components/landingpage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import "../public/css/landingPageStyle.css";
 import logo from "../public/resources/logo/logotoai_smooth.svg"
+import { Fade, Slide } from "react-awesome-reveal";
 
 function LandingPage() {
   return (
@@ -9,19 +10,32 @@ function LandingPage() {
       <nav className="landingPage-navbar">
         <Link to="/login" className="landingPage-iniciarSesion">Iniciar Sesión</Link>
       </nav>
+      
       <section className="landingPage-titles">
-        <div className="landingPage-byProjectersTitle">BY PROJECTERS</div>
-        <div className="landingPage-butterstockTitle">
-          <h2>ButterStock</h2>
-        </div>
-        <div className="landingPage-divLogo">
-          <img className="landingPage-logo" src={logo} alt="logo" />
-        </div>
+        <Fade delay={300}>  
+          <div className="landingPage-byProjectersTitle">BY PROJECTERS</div>
+        </Fade>
+
+        <Fade delay={100}>
+          <div className="landingPage-butterstockTitle">
+            <Slide> 
+              <h2>ButterStock</h2>
+            </Slide>
+          </div>
+        </Fade>
+
+        <Fade delay={100}>
+          <div className="landingPage-divLogo">
+            <img className="landingPage-logo" src={logo} alt="logo" />
+          </div>
+        </Fade>
 
         <div className="landingPage-mainbutton">
           <button className="landingPage-stylebutton" type="button">Empieza ya</button>
         </div>
+        
       </section>
+      
     </section>
   );
 }
