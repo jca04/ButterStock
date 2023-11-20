f4796863881e5d8bf9da9e999e6e5757c6e1b736
rake3344 <jca6558@gmail.com>
2023-11-20 12:37:53 -0500

    cambio en la ruta del backend

--- server/src/controllers/estadoDeResultado.js
+++ server/src/controllers/estadoDeResultado.js
@@ -139,7 +139,6 @@ const getEdr = async (req, res) => {
       res.status(200).json({ message: "Hello world" });
     }
   } catch (error) {
-    console.log(error.message);
     res.status(500).json({ error: error.message });
   }
 };
